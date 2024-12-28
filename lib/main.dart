97843c2f969d54f69b55853b94cc328932ffdb73
rihab-nikh <rihab_nikh@outlook.fr>
2024-12-28 18:12:05 +0100

    fixing logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-goutimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'Screens/home.page.dart';
